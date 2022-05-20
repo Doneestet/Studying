72bb5313030364b8609e5f684a51117498be6aec
Dmitriy Korobkin <korobkindmytro@gmail.com>
2022-05-20 14:30:15 +0300

    finished first homework in scropt.ja

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 'use strict';
 
+let money = prompt('Ваш бюджет на месяц?', ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+let a1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    a2 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    a3 = prompt('Во сколько обойдется?', ''),
+    a4 = prompt('Во сколько обойдется?', '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.money/30);
+// console.log(appData);
+
+// “Введите обязательную статью расходов в этом месяце”
 
 // let arr = ['plum.png', 'orange.png']
 // console.log(arr[0]);
@@ -16,5 +39,5 @@
 // let answer = confirm("Are you sure?");
 // console.log(answer);
 
-let answer = prompt("Вам есть 18?", "Да");   // promt - всплывает окно, и можно кстановить ответ по умолчанию
-console.log(typeof(answer));
+// let answer = prompt("Вам есть 18?", "Да");   // promt - всплывает окно, и можно кстановить ответ по умолчанию
+// console.log(typeof(answer));
