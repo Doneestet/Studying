c2349b11598582fa5e10802c0f1268247016671b
Dmitriy Korobkin <korobkindmytro@gmail.com>
2022-05-20 12:48:18 +0300

    strange thing

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+// let arr = ['plum.png', 'orange.png']
+// console.log(arr[0]);
+// let person = {
+// 	name: 'John',    		//name: это ключ
+// age: 25,
+// isMarried:  false
+// };
+	
+// console.log(person.name); 		// выводит John
+// console.log(person['name']); 	// второй способ записи
+
+
+// let answer = confirm("Are you sure?");
+// console.log(answer);
+
+let answer = prompt("Вам есть 18?", "Да");   // promt - всплывает окно, и можно кстановить ответ по умолчанию
+console.log(typeof(answer));
