032c55fd3b5334a16c298e9b9abba68ffb2d5bb7
Dmitriy Korobkin <korobkindmytro@gmail.com>
2022-05-21 11:06:20 +0300

    lesson about function

---
+++ lesson3/script3.js
@@ -0,0 +1,27 @@
+"use strict"
+
+// let num = 20;
+
+// function showFirstMassage(text){
+//     alert(text);
+//     console.log(num)
+// }
+// showFirstMassage("hello there");
+// console.log(num);
+
+function calc(a,b) {
+    return(a + b);
+}
+
+console.log(calc(3,4));
+
+console.log(calc(8,4));
+
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+
+let anotherVar = retVar();
+console.log(anotherVar);
