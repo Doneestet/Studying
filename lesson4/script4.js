6d98bbcd41f5ffaa8728e04d324dbc9adf9eb3f5
Dmitriy Korobkin <korobkindmytro@gmail.com>
2022-05-21 19:30:15 +0300

    Function - constructor

--- lesson4/script4.js
+++ lesson4/script4.js
@@ -67,8 +67,117 @@
 // arr.shift();  // удаляет первый элемент
 // arr.unshift('1'); // добавляет элемент в начало массва
 
-let ans = prompt("", ""),
-    arr = [];
+// let ans = prompt("", ""),  // спрашиваем ввести что-то
+//     arr = [];   //создаем массив
 
-arr = ans.split(",");
-console.log(arr);
+// arr = ans.split(",");  // разделяет запятой жлементы
+// console.log(arr);
+
+// let arr = ["asdasd", 'wfwefw', 'ewwef', '23fre'];
+//     i = arr.join(", ");  // склеивает строки
+
+// console.log(i);
+
+// let arr = [1, 15, 4];
+//     i = arr.sort(compareNum);  
+
+// function compareNum(a,b){  // callback функция
+//     return a - b;         
+// }
+// console.log(i);
+
+//  в предыдущем примере выведе 1, 15, 4
+// с обратной функцией 1, 4, 15
+
+// let soldier = {
+//     health: 400,
+//     armor: 100
+// };
+
+// let John = {
+//     health: 100
+// };
+// John.__proto__ = soldier;
+
+// console.log(John);
+// console.log(John.armor);
+
+// function BigUser() {
+
+//     this.name = "Вася";
+  
+//     return { name: "Godzilla" };  // <-- возвращает этот объект
+//   }
+  
+// alert( new BigUser().name ); 
+
+// Создайте функцию-конструктор Calculator, который создаёт объекты с тремя методами:
+
+// read() запрашивает два значения при помощи prompt и сохраняет их значение в свойствах объекта.
+// sum() возвращает сумму введённых свойств.
+// mul() возвращает произведение введённых свойств.
+
+
+// function Calculator(){
+//     this.read = function () {
+//         this.a = +prompt('a?', 0),
+//         this.b = +prompt('b?', 0);
+//     };
+//     this.sum = function () {
+//         return this.a + this.b;
+//     };
+//     this.mul = function () {
+//         return this.a * this.b;
+//     };
+// }
+// let calculator = new Calculator();
+// calculator.read();
+
+
+// alert('Sum= ' + calculator.sum() );
+// alert('Mul= ' + calculator.mul());
+
+// function Calculator(){
+//     this.read = function () {
+//         this.a = +prompt('a?', 0),
+//         this.b = +prompt('b?', 0);
+//     };
+//     this.sum = function () {
+//         return this.a + this.b;
+//     };
+//     this.mul = function () {
+//         return this.a * this.b;
+//     };
+// }
+// let calculator = new Calculator();
+// calculator.read();
+
+
+// alert('Sum= ' + Calculator.sum() );
+// alert('Mul= ' + Calculator.mul());
+
+// Напишите функцию-конструктор Accumulator(startingValue).
+
+// Объект, который она создаёт, должен уметь следующее:
+
+// Хранить «текущее значение» в свойстве value. Начальное значение устанавливается в аргументе конструктора startingValue.
+// Метод read() использует prompt для получения числа и прибавляет его к свойству value.
+// Таким образом, свойство value является текущей суммой всего, что ввёл пользователь при вызовах метода read(), с учётом начального значения startingValue.
+
+
+
+function Accumulator(startingValue) {
+    this.value = startingValue;
+    this.read = function () {
+        this.value += +prompt(' Сколько добавить?', 0);
+    };
+}
+
+let accumulator = new Accumulator(2);
+
+accumulator.read();
+accumulator.read();
+
+console.log(accumulator);
+alert(accumulator.value);
+
