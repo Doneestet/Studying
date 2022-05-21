70868ee4fea175171c66444479771093d5f140a4
Dmitriy Korobkin <korobkindmytro@gmail.com>
2022-05-21 17:34:23 +0300

    lesson about different methods like split,join,sort

--- lesson4/script4.js
+++ lesson4/script4.js
@@ -20,24 +20,55 @@
 // }
 // learnJS("JavaScript", done);
     
-let options = {
-    width: 100,
-    height: 100,
-    name: "test"
-};
+// let options = {
+//     width: 100,
+//     height: 100,
+//     name: "test"
+// };
 
 
 
-options.bool = false;
-options.colors = {
-    border: "black",
-    bg: "red"
-};
+// options.bool = false;
+// options.colors = {
+//     border: "black",
+//     bg: "red"
+// };
 
-delete options.bool; // удаляет свойство в объекте
-console.log(options);
+// delete options.bool; // удаляет свойство в объекте
+// console.log(options);
 
-for (let key in options){
-    console.log("Свойство " + key + " Имеет значение " + options[key]);
-}
-console.log(Object.keys(options).length);
+// for (let key in options){
+//     console.log("Свойство " + key + " Имеет значение " + options[key]);
+// }
+// console.log(Object.keys(options).length);  // конструкция для взаимодействия с объектом
+
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.forEach(function(item, i, mass) {       //item - каждый элем; i - номер; mass-сам масив, название любое
+//     console.log(i + ": " + item + "(массив: " + mass + ')');
+// });
+
+// console.log(arr);
+
+
+
+// let mass = [1, 2, 4, 6, 7];     
+
+// for(let key of mass) {      // перебор значений, не работает в объектах
+//     console.log(key);
+// }
+
+// for (let i = 0; i < arr.length; i++){  // способ перебрать все значения массива
+//     console.log(arr[i]);        
+
+// }
+// arr.pop();  // удалить элемент с конца массива
+// arr.push('5'); // добавить элемент в массив
+// arr.shift();  // удаляет первый элемент
+// arr.unshift('1'); // добавляет элемент в начало массва
+
+let ans = prompt("", ""),
+    arr = [];
+
+arr = ans.split(",");
+console.log(arr);
