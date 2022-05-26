66256d840030f1cfdff936b886abeb6f8f7285a4
Dmitriy Korobkin <korobkindmytro@gmail.com>
2022-05-26 20:03:30 +0300

    lesson about scroll and screen

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,56 +1,8 @@
-// 1 Задаем событие обработки сайта , лучше использовать  DOMLoader
-// window.addEventListener("DOMContentLoaded", function(){
-//     'use strict';
-//     // Задаем переменные
-
-//     let tab = document.querySelectorAll(".info-header-tab"),
-//         info = document.querySelector("info-header"),
-//         tabContent = document.querySelectorAll("info-tabcontent");
-
-//     //1 функция скрыть все табы после первого
-//     function hideTabContent(a) {
-//         for(let i = 0; i < tabContent.length; i++){
-//             tabContent[i].classList.remove("show");
-//             tabContent[i].classList.add('hide');
-//         }
-//     }
-//     hideTabContent(1); // вызываю функцию с 1 элемента
-
-//     function showTabContent(b) {
-//         if(tabContent[b].classList.contains('hide')){
-//             tabContent[b].classList.remove("hide");
-//             tabContent[b].classList.add('show');
-//          }
-//     }
-//     info.addEventListener('click', function(event){
-//         let target = event.target;
-//         if(target && event.target.classList.contains('info-header-tab')){
-//             for(let i = 0; i < tab.length; i++){
-//                 if(target == tab[i]){
-//                     hideTabContent(0);
-//                     showTabContent(i);
-//                     break;
-//                 }
-//             }
-//         }
-//     });
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
 window.addEventListener("DOMContentLoaded", function(){ // это событие срабатывает кога полностью загрузилась структура
 
     'use strict';
+    //задаем переменные
+    
     let tab = document.querySelectorAll(".info-header-tab"),
         info = document.querySelector(".info-header"),
         tabContent = document.querySelectorAll(".info-tabcontent");
