a25b4bd2f6c2febab4c837933c7f5455c853070b
Dmitriy Korobkin <korobkindmytro@gmail.com>
2022-05-27 11:29:10 +0300

    done timer

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -35,4 +35,104 @@ window.addEventListener("DOMContentLoaded", function(){ // это событие
             }
         }
     });
-}); 
+
+    // Timer 
+
+    let deadline = '2022-05-29';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+            
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num){
+                        if(num <= 9) {
+                            return '0' + num;
+                        } else return num;
+                    };
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+
+    }
+
+    setClock('timer', deadline);
+});
+
+
+
+//     let deadline = '2018-11-21';
+
+//     function getTimeRemaining(endtime) {
+//         let t = Date.parse(endtime) - Date.parse(new Date()),
+//         seconds = Math.floor((t/1000) % 60),
+//         minutes = Math.floor((t/1000/60) % 60),
+//         hours = Math.floor((t/(1000*60*60)));
+            
+//             // возвращает целый объект с данными
+//             return {  
+//                 "total" : t,
+//                 "hours" : hours,
+//                 "minutes" : minutes,
+//                 "seconds" : seconds 
+//             };
+//     }
+
+//     function setClock(id, endtime){  // функция создает различные переменные
+//         let timer =  document.getElementById(id),
+//             hours = document.querySelector('.hours'),
+//             minutes = document.querySelector('.minutes'),
+//             seconds = document.querySelector('.seconds');
+//             timeInterval = setInterval(updateClock, 1000); //вызываем функцию updateClock каждую секунду
+
+//         function updateClock(){
+//             let t = getTimeRamaining(endtime);  // сначала получает разницу между временем
+
+//             function addZero(num){
+//                 if(num <= 9) {
+//                     return '0' + num;
+//                 } else return num;
+//             };
+
+//             hours.textContent = addZero(t.hours);
+//             minutes.textContent = addZero(t.minutes);
+//             seconds.textContent = addZero(t.seconds);
+
+//             if(t.total <= 0){
+//                 clearInterval(timeInterval);  // условие останавливает таймер по достижению условия
+//                 hours.textContent = '00';
+//                 minutes.textContent = '00';
+//                 seconds.textContent = '00';
+//             }
+//         }
+//     }
+//     setClock('timer', deadline);
+// }); 
